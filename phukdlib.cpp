3c0f0bb47a45c8f308de3b5de5702af4ba15b2c2
Ward Wouts <ward@wouts.nl>
2015-12-08 12:27:30 +0100

    type slowly for OS X

--- phukdlib.cpp
+++ phukdlib.cpp
@@ -77,7 +77,7 @@ void CommandAtRunBarOSX(const char *SomeCommand){
   Keyboard.set_key1(0); //have to do this to keep it from hitting key multiple times.
   Keyboard.send_now(); //Send the key changes
   delay(1000);
-  Keyboard.print(SomeCommand);
+  SlowType(SomeCommand, 50);
   delay(1000);
   Keyboard.set_key1((uint8_t)KEY_ENTER);
   Keyboard.send_now();
@@ -99,7 +99,7 @@ void CommandAtNewTerminal(const char *SomeCommand){
   Keyboard.set_key1(0); //have to do this to keep it from hitting key multiple times.
   Keyboard.send_now(); //Send the key changes
   delay(1500);
-  Keyboard.print(SomeCommand);
+  SlowType(SomeCommand, 50);
   Keyboard.set_key1((uint8_t)KEY_ENTER);
   Keyboard.send_now();
   Keyboard.set_key1(0);
